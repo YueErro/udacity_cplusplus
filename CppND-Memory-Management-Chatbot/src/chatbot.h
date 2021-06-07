427d49ac4b86140f3f40229a22d1ad2f09ad7e28
YueErro <yue.trbj@gmail.com>
2021-06-07 21:53:53 +0200

    style: Apply clang format to chatbot project

--- CppND-Memory-Management-Chatbot/src/chatbot.h
+++ CppND-Memory-Management-Chatbot/src/chatbot.h
@@ -4,44 +4,56 @@
 #include <wx/bitmap.h>
 #include <string>
 
-class GraphNode; // forward declaration
-class ChatLogic; // forward declaration
+class GraphNode;  // forward declaration
+class ChatLogic;  // forward declaration
 
 class ChatBot
 {
 private:
-    // data handles (owned)
-    wxBitmap *_image; // avatar image
+  // data handles (owned)
+  wxBitmap *_image;  // avatar image
 
-    // data handles (not owned)
-    GraphNode *_currentNode;
-    GraphNode *_rootNode;
-    ChatLogic *_chatLogic;
+  // data handles (not owned)
+  GraphNode *_currentNode;
+  GraphNode *_rootNode;
+  ChatLogic *_chatLogic;
 
-    // proprietary functions
-    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+  // proprietary functions
+  int ComputeLevenshteinDistance(std::string s1, std::string s2);
 
 public:
-    // constructors / destructors
-    ChatBot();                     // constructor WITHOUT memory allocation
-    ChatBot(std::string filename); // constructor WITH memory allocation
-    ~ChatBot();
+  // constructors / destructors
+  ChatBot();                      // constructor WITHOUT memory allocation
+  ChatBot(std::string filename);  // constructor WITH memory allocation
+  ~ChatBot();
 
-    //// STUDENT CODE
-    ////
+  //// STUDENT CODE
+  ////
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 
-    // getters / setters
-    void SetCurrentNode(GraphNode *node);
-    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
-    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
-    ChatLogic* GetChatLogicHandle() { return _chatLogic; }
-    wxBitmap *GetImageHandle() { return _image; }
+  // getters / setters
+  void SetCurrentNode(GraphNode *node);
+  void SetRootNode(GraphNode *rootNode)
+  {
+    _rootNode = rootNode;
+  }
+  void SetChatLogicHandle(ChatLogic *chatLogic)
+  {
+    _chatLogic = chatLogic;
+  }
+  ChatLogic *GetChatLogicHandle()
+  {
+    return _chatLogic;
+  }
+  wxBitmap *GetImageHandle()
+  {
+    return _image;
+  }
 
-    // communication
-    void ReceiveMessageFromUser(std::string message);
+  // communication
+  void ReceiveMessageFromUser(std::string message);
 };
 
 #endif /* CHATBOT_H_ */
--- CppND-Memory-Management-Chatbot/src/graphedge.cpp
+++ CppND-Memory-Management-Chatbot/src/graphedge.cpp
@@ -3,20 +3,20 @@
 
 GraphEdge::GraphEdge(int id)
 {
-    _id = id;
+  _id = id;
 }
 
 void GraphEdge::SetChildNode(GraphNode *childNode)
 {
-    _childNode = childNode;
+  _childNode = childNode;
 }
 
 void GraphEdge::SetParentNode(GraphNode *parentNode)
 {
-    _parentNode = parentNode;
+  _parentNode = parentNode;
 }
 
 void GraphEdge::AddToken(std::string token)
 {
-    _keywords.push_back(token);
+  _keywords.push_back(token);
 }
