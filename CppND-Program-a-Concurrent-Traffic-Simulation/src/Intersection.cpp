7c4d1acde8dd0cf4cfd74eb6ee523b0f727b58db
YueErro <yue.trbj@gmail.com>
2021-07-11 16:02:03 +0200

    feat: Add Task FP.6

--- CppND-Program-a-Concurrent-Traffic-Simulation/src/Intersection.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation/src/Intersection.cpp
@@ -88,8 +88,10 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
   std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID()
             << " is granted entry." << std::endl;
 
-  // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen
-  // to block the execution until the traffic light turns green.
+  if (_trafficLight.getCurrentPhase() != TrafficLightPhase::GREEN)
+  {
+    _trafficLight.waitForGreen();
+  }
 
   lck.unlock();
 }
@@ -111,9 +113,7 @@ void Intersection::setIsBlocked(bool isBlocked)
 // virtual function which is executed in a thread
 void Intersection::simulate()  // using threads + promises/futures + exceptions
 {
-  // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight.
-  // At this position, start the simulation of _trafficLight.
-
+  _trafficLight.simulate();
   // launch vehicle queue processing in a thread
   threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
 }
